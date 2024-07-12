0feb5c7db44ebbca10817b6150c4150138236d4a
JasnDev <148272196+JasnDev@users.noreply.github.com>
2024-07-12 16:06:04 -0300

    Update db.js

--- db.js
+++ db.js
@@ -5,12 +5,7 @@ async function db (){
     if (global.connection && global.connection.state !== "disconnected")
         return global.connection;
 
-    const conn = await mysql.createConnection({host:"formulario-girlscode.mysql.database.azure.com", 
-        user:"j4sanias", 
-        password:"Admin123", 
-        database:"formulario", 
-        port:3306,  
-    });
+    const conn = await mysql.createConnection(process.env.DATABASE_CONNECTION_STRING);
 
     console.log("Conectado ao MySQL!");
      global.connection = conn;
